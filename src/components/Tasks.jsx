709ff7ed0cafd6a0e2bc32744d755c0f5e13343b
Pedro Henrique <80182753+Peedrohenrique@users.noreply.github.com>
2021-09-18 16:36:47 -0300

    finalizar Task
    Só pra finalizar ou tirar a finalização da task

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 import Task from "./Task";
 
-const Tasks = ({tasks}) => {
-    return(
+const Tasks = ({tasks, handleTaskClick }) => {
+    return (
         <>
-            {tasks.map(task => <Task task={task}/>)}
+            {tasks.map((task) => (
+                 <Task task={task} handleTaskClick={handleTaskClick} />
+            ))}
         </>
     )
 };
